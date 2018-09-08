3d8e162c1cd2dd3ce212efb3098eebfb32cf679b
Chuck Okonkwo <chukwukaaguolu@gmail.com>
2018-09-08 16:01:27 -0400

    real initial commit

---
+++ test/test-default.js
@@ -0,0 +1,31 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const { app, runServer, closeServer } = require("../server");
+const faker = require('faker');
+const mongoose = require('mongoose');
+
+const expect = chai.expect;
+
+
+chai.use(chaiHttp);
+
+
+describe('Initial tests', function() {
+
+  before(function(){
+    return runServer;
+  });
+
+  after(function(){
+    return closeServer;
+  });
+
+	it('should serve static html files and return a 200 status code upon server start', function() {
+			return chai.request(app).get('/').then(function(res) {
+				expect(res).to.have.status(200);
+				expect(res).to.be.html;
+		})
+
+	})
+
+});
